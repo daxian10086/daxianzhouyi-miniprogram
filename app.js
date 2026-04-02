a1011d538d94ded1b6c1dfe1acad4f2d3eefe9b2
daxian10086 <daxian10086@github.com>
2026-04-02 10:44:14 +0800

    v2.4.146 - 统一版本号

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ App({
 
   globalData: {
     userInfo: null,
-    version: 'v2.4.144',
+    version: 'v2.4.145',
     versionDate: '2026-04-01'
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ const hexagramMainFortunes = require('../../data/hexagramMainFortunes.js');
 
 Page({
   data: {
-    version: 'v2.4.144',
+    version: 'v2.4.145',
     isAnimating: false,
     hexagram: null,
     currentTab: 0,
